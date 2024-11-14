4002460076ce3658c11c4d9dc89306ae2c8fa734
Flavio Avdulla <a.flavio4366@gmail.com>
2024-11-14 09:45:22 +0000

    3-currency.js

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,38 +1,28 @@
-export default class Currency {
+class Currency {
   constructor(code, name) {
-    if (typeof name !== 'string') {
-      throw TypeError('Name must be a string');
-    }
-    if (typeof code !== 'string') {
-      throw TypeError('Code must be a string');
-    }
-    this._name = name;
-    this._code = code;
-  }
-
-  set name(name) {
-    if (typeof name !== 'string') {
-      throw TypeError('Code must be a string');
-    }
-    this._name = name;
-  }
-
-  set code(code) {
-    if (typeof code !== 'string') {
-      throw TypeError('Code must be a string');
-    }
-    this._code = code;
-  }
-
-  get name() {
-    return this._name;
+    this.code = code;
+    this.name = name;
   }
 
   get code() {
     return this._code;
   }
 
+  set code(code) {
+    if (typeof code === 'string') this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof name === 'string') this._name = name;
+  }
+
   displayFullCurrency() {
-    return `${this._name} (${this._code})`;
+    return `${this.name} (${this.code})`;
   }
 }
+
+export default Currency;
