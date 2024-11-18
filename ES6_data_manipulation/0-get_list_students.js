1d04cc25800ea0d02c0ea7e1a2ee11187f4a19b9
Flavio Avdulla <a.flavio4366@gmail.com>
2024-11-18 12:48:18 +0000

    0-get_list_students.js

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,21 @@
-const getListStudents = () => {
-  const students = [
-    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' }
-  ]
+export default function getListStudents() {
+  const person1 = {
+      id: 1,
+      firstName: 'Guillaume',
+      location: 'San Francisco',
+    };
+
+    const person2 = {
+        id: 2,
+        firstName: 'James',
+        location: 'Columbia',
+    };
+
+    const person3 = {
+        id: 5,
+        firstName: 'Serena',
+        location: 'San Francisco',
+      };
+
+    return [person1, person2, person3]
 }
