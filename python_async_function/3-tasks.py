ebd981e9bda40e4b380b8b8952a9d7435a57d561
Flavio Avdulla <a.flavio4366@gmail.com>
2024-12-06 20:53:05 +0000

    3-tasks.py

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -10,7 +10,7 @@ import asyncio
 wait_random = __import__("0-basic_async_syntax").wait_random
 
 
-def task_wait_random(max_delay:int = 10) -> asyncio.Task:
+def task_wait_random(max_delay: int = 10) -> asyncio.Task:
     """
     Parameters
         max_delay: int, default 10
@@ -22,5 +22,5 @@ def task_wait_random(max_delay:int = 10) -> asyncio.Task:
     return asyncio.create_task(wait_random(max_delay))
 
 
-if __name__ == "__main__"
+if __name__ == "__main__":
     task_wait_random(5)
