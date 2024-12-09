72fd846e2a7f42d5998b1adafcfdddf47e505419
Flavio Avdulla <a.flavio4366@gmail.com>
2024-12-09 18:31:41 +0000

    2-measure_runtime.py

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
-"""
-asyncio.gather
-"""
+""" asyncio.gather """
 
 import asyncio
 import random
@@ -9,17 +7,15 @@ import timeit
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def meassure_runtime() -> float:
-    """
-    Executes comprehension func 4 times in a parallel, returns runtime.
-    """
+async def measure_runtime() -> float:
+    """ executes comprehension func 4 times in parallel, returns runtime """
 
     start = timeit.default_timer()
     await asyncio.gather(
         async_comprehension(),
         async_comprehension(),
         async_comprehension(),
-        async_comprehension(),
+        async_comprehension()
         )
     stop = timeit.default_timer()
     return stop - start
