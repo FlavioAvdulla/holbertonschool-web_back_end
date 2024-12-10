9ded320adbc691e21bf833d31d555da44ee188eb
Flavio Avdulla <a.flavio4366@gmail.com>
2024-12-10 21:19:43 +0000

    0-simple_helper_function.py

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python3
-""" This module provides a method for calculating
-the start and end indices for paginated data.
-Typical usage example: index_range(page, page_size) """
+""" Pagination """
 
 from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """ returns tuple of size two containing a
-    start index and an end index """
+    """ returns tuple of size two containing a start index and an end index """
     pass
